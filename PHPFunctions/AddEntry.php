cede688136ac8dc0ad5ab64d7577ff481881e7e7
Ricardo Buchebner <ricardo.Buchebner@liebherr.com>
2023-06-21 15:05:43 +0200

    Add Entry now working

--- PHPFunctions/AddEntry.php
+++ PHPFunctions/AddEntry.php
@@ -11,24 +11,23 @@ $preis = $_POST['preis'];
 $farbe = $_POST['farbe'];
 $kilometerstand = $_POST['kilometerstand'];
 $erstzulassung = $_POST['erstzulassung'];
-$land = $_POST['land'];
 
 // SQL-Abfrage zum Einfügen des neuen Herstellers, falls er noch nicht existiert
-$sql = "INSERT IGNORE INTO Hersteller (Name, Land) VALUES ('$herstellername', '$land')";
+$sql = "INSERT IGNORE INTO Hersteller (Name) VALUES ('$herstellername')";
 
 if ($conn->query($sql) === TRUE) {
     $herstellerID = $conn->insert_id;
 } else {
-    $herstellerID = mysqli_fetch_assoc(mysqli_query($conn, "SELECT Hersteller_ID FROM Hersteller WHERE Name = '$herstellername'"))['Hersteller_ID'];
+    $herstellerID = mysqli_fetch_assoc(mysqli_query($conn, "SELECT HerstellerID FROM Hersteller WHERE Name = '$herstellername'"))['Hersteller_ID'];
 }
 
 // SQL-Abfrage zum Einfügen des neuen Modells, falls es noch nicht existiert
-$sql = "INSERT IGNORE INTO Modell (Modellname, Hersteller_ID, Baujahr, Preis) VALUES ('$modellname', $herstellerID, $baujahr, $preis)";
+$sql = "INSERT IGNORE INTO Modell (Modellname, HerstellerID, Baujahr, Preis) VALUES ('$modellname', $herstellerID, $baujahr, $preis)";
 
 if ($conn->query($sql) === TRUE) {
     $modelID = $conn->insert_id;
 } else {
-    $modelID = mysqli_fetch_assoc(mysqli_query($conn, "SELECT ModellID FROM Modell WHERE Modellname = '$modellname' AND Hersteller_ID = $herstellerID"))['ModellID'];
+    $modelID = mysqli_fetch_assoc(mysqli_query($conn, "SELECT ModellID FROM Modell WHERE Modellname = '$modellname' AND HerstellerID = $herstellerID"))['ModellID'];
 }
 
 // SQL-Abfrage zum Einfügen des neuen Autos
